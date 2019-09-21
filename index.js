bd7d2881f62bed302b4fda8cd8a61462652eecde
AloeCola <onetuks@naver.com>
2019-09-21 23:07:03 +0900

    여러가지 middleware 이용

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 import express from "express";
+import morgan from "morgan";
+//앞의 morgan은 변수로 이름을 원하는대로 바꿀 수 있다.
+import helmet from "helmet";
+//helmet은 보안을 위한것, 쓰면 좋은거
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+
 const app = express();
 
 const PORT = 4000;
@@ -10,17 +17,13 @@ const handleHome = (req, res) => res.send("Hello from ass");
 
 const handleProfile = (req, res) => res.send("You are on my profile");
 
-const betweenHome = (req, res, next) => {
-    console.log("Between");
-    next();
-};  //log에 뭔가를 남기고 싶다면 middleware을 사용하면 된다.
+app.use(cookieParser);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(helmet());
+app.use(morgan("dev"));
 
-app.use(betweenHome);
-//middleware을 다 처리한 후에 route를 처리해야한다.
-//무조건 코드의 위치에 따라 위에서 아래 순으로 실행
-//지금은 모든 route에 적용된다.
-
-app.get("/", betweenHome, handleHome);
+app.get("/", handleHome);
 
 app.get("/profile", handleProfile);
 
