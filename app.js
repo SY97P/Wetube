0e28e719fa99721cd9f0672e5b48658b505e5579
AloeCola <onetuks@naver.com>
2019-09-21 23:39:54 +0900

    router사용과 export&import

--- app.js
+++ app.js
@@ -5,21 +5,19 @@ import helmet from "helmet";
 //helmet은 보안을 위한것, 쓰면 좋은거
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import { userRouter } from "./router";
+//다른 import들은 애초에 default이기 때문에 여기서 내맘대로 이름을 바꿔서 쓸 수 있지만
+//userRouter는 router.js에서 export const userRouter로 선언했기 때문에 여기서 이렇게 app.js에서 사용해야한다.
 
 const app = express();
-
-const PORT = 4000;
-
-const handleListening = () =>
-    console.log(`Listening on: http://localhost:${PORT}`); 
 
 const handleHome = (req, res) => res.send("Hello from ass");
 
 const handleProfile = (req, res) => res.send("You are on my profile");
 
-app.use(cookieParser);
+app.use(cookieParser());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(morgan("dev"));
 
@@ -27,5 +25,8 @@ app.get("/", handleHome);
 
 app.get("/profile", handleProfile);
 
+app.use("/user", userRouter);
 
-app.listen(PORT, handleListening);
+export default app;
+//누군가 내 파일을 불러올때(import) 
+//app object(app.**)를 주겠다는 의미
